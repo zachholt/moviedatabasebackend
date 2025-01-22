ecc876531058a0cebbc2901f24236a8232a82d30
Zach <zacjac96@icloud.com>
2025-01-21 20:13:28 -0600

    updated properties

--- src/main/java/com/zachholt/MovieDatabaseAPI/services/RatingService.java
+++ src/main/java/com/zachholt/MovieDatabaseAPI/services/RatingService.java
@@ -15,19 +15,19 @@ public class RatingService {
         this.ratingRepository = ratingRepository;
     }
 
-    @PostConstruct
-    public void initializeRatings() {
-        if (ratingRepository.count() == 0) {
-            List<Rating> standardRatings = Arrays.asList(
-                createRating(1, "G", "General Audience"),
-                createRating(2, "PG", "Parental Guidance Suggested"),
-                createRating(3, "PG-13", "Parents Strongly Cautioned"),
-                createRating(4, "R", "Restricted"),
-                createRating(5, "NC-17", "Adults Only")
-            );
-            ratingRepository.saveAll(standardRatings);
-        }
-    }
+//    @PostConstruct
+//    public void initializeRatings() {
+//        if (ratingRepository.count() == 0) {
+//            List<Rating> standardRatings = Arrays.asList(
+//                createRating(1, "G", "General Audience"),
+//                createRating(2, "PG", "Parental Guidance Suggested"),
+//                createRating(3, "PG-13", "Parents Strongly Cautioned"),
+//                createRating(4, "R", "Restricted"),
+//                createRating(5, "NC-17", "Adults Only")
+//            );
+//            ratingRepository.saveAll(standardRatings);
+//        }
+//    }
 
     private Rating createRating(Integer id, String rating, String description) {
         Rating r = new Rating();
