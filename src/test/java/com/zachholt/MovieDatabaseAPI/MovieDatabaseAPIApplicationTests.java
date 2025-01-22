638b1b5e432e4386b5851fd2be42608f5e9bc6ca
Zach <zacjac96@icloud.com>
2025-01-21 19:48:27 -0600

    commented out tests

--- src/test/java/com/zachholt/MovieDatabaseAPI/MovieDatabaseAPIApplicationTests.java
+++ src/test/java/com/zachholt/MovieDatabaseAPI/MovieDatabaseAPIApplicationTests.java
@@ -3,10 +3,10 @@ package com.zachholt.MovieDatabaseAPI;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MovieDatabaseAPIApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
